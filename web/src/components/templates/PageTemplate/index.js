c3d00193154550764f1a65ecefc8d5dc974a6841
Mattia Natali <matitalatina@gmail.com>
2017-12-28 18:18:13 +0100

    Add favicon, font for title, add footer

--- web/src/components/templates/PageTemplate/index.js
+++ web/src/components/templates/PageTemplate/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { size } from 'styled-theme'
+import logoImg from '../../../resources/tire-icon.png'
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding-top: 3.75rem;
+  padding-top: 1rem;
   min-height: 100vh;
   box-sizing: border-box;
   @media screen and (max-width: 640px) {
@@ -14,11 +15,13 @@ const Wrapper = styled.div`
   }
 `
 
-const Header = styled.header`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 999;
+const Header = styled.header``
+
+const HeaderWrapper = styled.div`
+  max-width: 1100px;
+  margin: auto;
+  font-family: 'Audiowide';
+  font-size: 30pt;
 `
 
 const Content = styled.section`
@@ -30,14 +33,45 @@ const Content = styled.section`
 
 const Footer = styled.footer`
   margin-top: auto;
+  background-color: #222;
+  color: #aaa;
+`
+
+const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 2rem 0;
+  font-size: 7pt;
+`
+
+const Logo = styled.img`
+  height: 50px;
+  margin-right: 1rem;
 `
 
 const PageTemplate = ({
   header, children, footer, ...props
 }) => {
+  if (!header) {
+    header = (
+      <HeaderWrapper>
+        <Logo src={logoImg} />
+        Röda Project
+      </HeaderWrapper>
+    )
+  }
+  if (!footer) {
+    footer = (
+      <FooterWrapper>
+        Made with&nbsp;<span role="img" aria-label="heart">❤️</span>&nbsp;by Mattia Natali
+      </FooterWrapper>
+    )
+  }
   return (
     <Wrapper {...props}>
+      <Header>{header}</Header>
       <Content>{children}</Content>
+      <Footer>{footer}</Footer>
     </Wrapper>
   )
 }
