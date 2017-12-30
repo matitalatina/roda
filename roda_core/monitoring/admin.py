4491be65fa20410e598cd6e5d7254b2ca58c65c2
Mattia Natali <matitalatina@gmail.com>
2017-12-30 11:50:16 +0100

    Improve Admin panel

--- roda_core/monitoring/admin.py
+++ roda_core/monitoring/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from monitoring.models import TyreMeasurement
 
-admin.site.register(TyreMeasurement)
+
+@admin.register(TyreMeasurement)
+class TyreMeasurementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'car', 'timestamp', 'position',)
+    list_filter = ('car', 'position',)
+    search_fields = ('car',)
+    ordering = ('car', '-timestamp',)
